e12ef368eadd82baf845a6bb45e9dbd8a6ee2b08
BingCheng <b09901044@ntu.edu.tw>
2023-08-20 12:37:42 +0800

    first commit

---
+++ SerialCapture.py
@@ -0,0 +1,23 @@
+import serial
+import time
+# terminal:　python3 -m serial.tools.miniterm /dev/ttyUSB0 115200
+
+class UWB3000Serial(serial.Serial):
+    def __init__(self, path, baudrate):
+        super().__init__(path, baudrate)
+    
+    def read_distance(self):
+        """Returns the distance of 2 tags in meters.
+        """
+        self.write(b"Read Distance!\n")
+        text = self.readline().decode('ascii')
+        return text[6:-3]
+
+
+if __name__ == '__main__':
+    ser = UWB3000Serial('/dev/ttyUSB0',115200)
+    ser.reset_input_buffer()
+    while True: 
+        print(ser.read_distance())
+        time.sleep(0.1)
+    ser.close()
